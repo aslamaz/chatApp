be0d15ff0283eb62a3ae085d535f3163ce8aff82
Aslam <muhammedazeez473@gmail.com>
2025-03-12 11:45:31 +0530

    changes

--- quickChat-app/vite.config.js
+++ quickChat-app/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Allows Render to detect the open port
+    port: 3000        // Use a fixed port if needed
+  }
 })
